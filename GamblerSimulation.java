8624c070084d38619c7a4153addc2fb77e0be4f8
nithunkarur <rknithun@gmail.com>
2021-10-20 09:21:14 +0530

    ADDED: Two variable for defining daily stake and bet amount for each game

---
+++ GamblerSimulation.java
@@ -0,0 +1,13 @@
+class GamblerSimulation{
+
+	int everyDayStake=100;
+	public void startGamble(){
+		int everybet=1;
+	}
+
+	public static void main(String[] args){
+		System.out.println("welcome to gambling");
+		GamblerSimulation gamble=new GamblerSimulation();
+		gamble.startStake();
+	}
+}
